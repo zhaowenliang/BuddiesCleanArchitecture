ad7ffbd7ca44e3f26798557efaa222de1b3bb76d
zhaowenliang <zwldh123@163.com>
2020-08-18 19:55:53 +0800

    修改个人中心页面。

--- app/src/main/java/cc/buddies/cleanarch/fragment/MineFragment.java
+++ app/src/main/java/cc/buddies/cleanarch/fragment/MineFragment.java
@@ -1,6 +1,7 @@
 package cc.buddies.cleanarch.fragment;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import cc.buddies.cleanarch.R;
+import cc.buddies.component.common.helper.StatusBarHelper;
 
 public class MineFragment extends Fragment {
 
@@ -24,5 +26,15 @@ public class MineFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        TypedValue typedValue = new TypedValue();
+        requireContext().getTheme().resolveAttribute(R.attr.colorSurface, typedValue, true);
+        int color = typedValue.data;
+
+        StatusBarHelper.tintStatusBar(requireContext(), requireActivity().getWindow(), color, false);
     }
 }
